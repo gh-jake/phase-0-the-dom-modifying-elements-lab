949024c85c59421009bb78bc6b7369dc7f26629c
Jake Houston <jakechouston@gmail.com>
2021-03-08 00:00:00 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let node = document.getElementById("main");
+node.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+const text = document.createTextNode("Jake is the champion");
+newHeader.appendChild(text);
